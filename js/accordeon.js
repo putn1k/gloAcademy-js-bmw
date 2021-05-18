56ad53d469fac0c51a2b36141caaf61265945303
putn1k <skachok11@yandex.ru>
2021-05-18 18:12:33 +0300

    выполняет дополнение к ДЗ №1

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,19 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const featureLinkElements = document.querySelectorAll('.feature__link');
-    const featureSubElements = document.querySelectorAll('.feature-sub');
-    featureLinkElements.forEach( (button, index) => {
-        button.addEventListener('click', () => {
+  const featureLinkElements = document.querySelectorAll('.feature__link');
+  const featureSubElements = document.querySelectorAll('.feature-sub');
+  featureLinkElements.forEach((button, index) => {
+    button.addEventListener('click', () => {
 
-            featureSubElements.forEach( (featureSubElement) => {
-                featureSubElement.classList.add('hidden');
-            });            
-            
-            featureLinkElements.forEach( (featureLinkElement) => {
-                featureLinkElement.classList.remove('feature__link_active');
-            });
-            
-            featureSubElements[index].classList.remove('hidden');
-            featureLinkElements[index].classList.add('feature__link_active');
+      if (featureLinkElements[index].classList.contains('feature__link_active')) {
+        featureLinkElements[index].classList.remove('feature__link_active');
+        featureSubElements[index].classList.add('hidden');
+      } else {
+        featureSubElements.forEach((featureSubElement) => {
+          featureSubElement.classList.add('hidden');
         });
+
+        featureLinkElements.forEach((featureLinkElement) => {
+          featureLinkElement.classList.remove('feature__link_active');
+        });
+        featureSubElements[index].classList.remove('hidden');
+        featureLinkElements[index].classList.add('feature__link_active');
+      }
     });
+  });
 });
